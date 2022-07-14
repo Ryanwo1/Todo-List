1121094666827a50d9b85218691cd8d7a56017aa
Ryan Odribege <rodribege@my.bcit.ca>
2022-07-14 00:53:21 -0300

    add button with eventlistener for adding new task

--- src/index.js
+++ src/index.js
@@ -3,13 +3,40 @@ import logoVersionSvg from "./img/todoist-seeklogo.com.svg";
 import logoVersionPng from "./img/todoist-logo.png";
 
 const logoSvg = document.getElementById("logo-svg");
-const logoPng = document.getElementById("logo-png");
+
 
 logoSvg.src = logoVersionSvg;
-logoPng.src = logoVersionPng;
+
+const mainAppTitle = document.getElementById("main-app-title");
+const addTaskButton = document.getElementById("new-task-button");
+
+function showInputFieldsForNewtask() {
+    const titleOfTaskInputField = document.createElement("input");
+    const titleTextParagraph = document.createElement("p");
+    titleTextParagraph.innerHTML = "Task Title";
+
+    titleOfTaskInputField.setAttribute("type", "text");
+    titleOfTaskInputField.setAttribute("id", "title-input-field");
+
+
+    const dueDateOfTaskInputField = document.createElement("input");
+    const dueDateTextParagraph = document.createElement("p");
+    dueDateTextParagraph.innerHTML = "Due Date";
+
+    dueDateOfTaskInputField.setAttribute("type", "text");
+    dueDateOfTaskInputField.setAttribute("id", "due-date-input-field");
+    dueDateTextParagraph.innerHTML = "Due Date";
+
+    mainAppTitle.append(titleOfTaskInputField);
+    mainAppTitle.append(titleTextParagraph);
+    mainAppTitle.append(dueDateOfTaskInputField);
+    mainAppTitle.append(dueDateTextParagraph);
+}
+
+addTaskButton.addEventListener("click", showInputFieldsForNewtask);
 
 let ryan = {
-    cool: "fsdsasd",
+    cool: "fsdsdasssssasd",
 }
 
 console.log(ryan);
