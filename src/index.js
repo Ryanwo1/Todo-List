bc42e6a812b5245adb9345c01c6a94d16f1675f9
Ryan Odribege <rodribege@my.bcit.ca>
2022-07-15 00:07:07 -0300

    create input boxes for a new task

--- src/index.js
+++ src/index.js
@@ -1,42 +1,115 @@
 import "./styles/index.css";
 import logoVersionSvg from "./img/todoist-seeklogo.com.svg";
-import logoVersionPng from "./img/todoist-logo.png";
 
 const logoSvg = document.getElementById("logo-svg");
-
-
 logoSvg.src = logoVersionSvg;
 
 const mainAppTitle = document.getElementById("main-app-title");
 const addTaskButton = document.getElementById("new-task-button");
+const toDoListMainSection = document.getElementById("todo-list-main-section");
 
-function showInputFieldsForNewtask() {
-    const titleOfTaskInputField = document.createElement("input");
-    const titleTextParagraph = document.createElement("p");
-    titleTextParagraph.innerHTML = "Task Title";
-
-    titleOfTaskInputField.setAttribute("type", "text");
-    titleOfTaskInputField.setAttribute("id", "title-input-field");
+const toDoListMainList = (function () {
+    let mainListInstance;
+    let numberOfTasks = 0;
+    let listOfCurrentTasks = [];
 
 
-    const dueDateOfTaskInputField = document.createElement("input");
-    const dueDateTextParagraph = document.createElement("p");
-    dueDateTextParagraph.innerHTML = "Due Date";
+    function createInstance() {
+        let object = new Object();
+        return object;
+    }
 
-    dueDateOfTaskInputField.setAttribute("type", "text");
-    dueDateOfTaskInputField.setAttribute("id", "due-date-input-field");
-    dueDateTextParagraph.innerHTML = "Due Date";
+    function addTask(individualTask) {
+        listOfCurrentTasks.push(individualTask);
+        this.numberOfTasks++;
+        
+        return numberOfTasks;
+    }
 
-    mainAppTitle.after(titleOfTaskInputField);
-    mainAppTitle.after(titleTextParagraph);
-    mainAppTitle.after(dueDateOfTaskInputField);
-    mainAppTitle.after(dueDateTextParagraph);
+    return {
+        numberOfTasks,
+        getInstance: function () {
+            if (!mainListInstance) {
+                mainListInstance = createInstance();
+            }
+            return mainListInstance;
+        },
+        addTask,
+        deleteTask: function(individualTask) {
+
+        },
+        showNumberOfTasks: function() {
+
+            console.log(this.numberOfTasks);
+        },
+        showTasks: function() {
+            console.log(listOfCurrentTasks);
+        }
+    };
+})();
+
+
+
+function makeIndividualTaskContainer() {
+    const individualTaskContainer = document.createElement("div");
+    individualTaskContainer.setAttribute("id", `task-number-${toDoListMainList.numberOfTasks}`)
+    individualTaskContainer.classList.add("individual-task")
+    return individualTaskContainer;
 }
 
-addTaskButton.addEventListener("click", showInputFieldsForNewtask);
+function makeindividualTaskPropertyContainer() {
+    const individualTaskNameContainer = document.createElement("div");
+    individualTaskNameContainer.classList.add(".task-to-be-added");
 
-let ryan = {
-    cool: "fsdsdasssssasd",
+    return individualTaskNameContainer;
 }
 
-console.log(ryan);
+function makeIndividualPropertyLabel(attributeName, attributeValue, buttonText) {
+    const individualPropertyLabel = document.createElement("label");
+    individualPropertyLabel.setAttribute(`${attributeName}`, `${attributeValue}`);
+    individualPropertyLabel.innerHTML = `${buttonText}`;
+
+    return individualPropertyLabel;
+}
+
+function createInputBox(attributeName, attributeValue) {
+    const inputBox = document.createElement("input");
+    inputBox.setAttribute(`${attributeName}`, `${attributeValue}`);
+
+    return inputBox;
+}
+
+function showInputFieldsForNewTask() {
+    const individualTaskContainerDiv = makeIndividualTaskContainer();
+    const individualTaskNameContainerDiv = makeindividualTaskPropertyContainer();
+    const individualTaskNameTitle = makeIndividualPropertyLabel("for", "task-name", "Task Name");
+    const individualTaskNameInputField = createInputBox("id", `task-name`);
+    const individualDueDateContainer = makeindividualTaskPropertyContainer();
+    const individualDueDateLabel = makeIndividualPropertyLabel("for", "due-date", "Due Date");
+    const individualDueDateInput = createInputBox("id", "due-date");
+
+    // append input box and label to container div
+    individualTaskNameContainerDiv.appendChild(individualTaskNameTitle);
+    individualTaskNameContainerDiv.appendChild(individualTaskNameInputField);
+
+    //append due date input box and label to individual due date container
+
+    individualDueDateContainer.appendChild(individualDueDateLabel);
+    individualDueDateContainer.appendChild(individualDueDateInput);
+
+    // append task name and due date containers to individual task container, forming an individual task on the page.
+
+    individualTaskContainerDiv.appendChild(individualTaskNameContainerDiv);
+    individualTaskContainerDiv.appendChild(individualDueDateContainer)
+
+    // append task to list
+    toDoListMainSection.append(individualTaskContainerDiv);
+
+    // ${toDoListMainList.numberOfTasks}
+    toDoListMainList.addTask(individualTaskContainerDiv);
+    toDoListMainList.showNumberOfTasks();
+    toDoListMainList.showTasks();
+}
+
+addTaskButton.addEventListener("click", showInputFieldsForNewTask);
+
